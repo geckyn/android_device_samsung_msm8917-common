61f390fa372480f653b87a55f06955eaef8be356
dianlujitao <dianlujitao@lineageos.org>
2019-09-16 17:40:51 +0800

    j4primelte: Create system copy of ims-ext-common and add to classpath
    Signed-off-by: geckyn <richardaffrim@gmail.com>

--- msm8917.mk
+++ msm8917.mk
@@ -392,7 +392,7 @@ PRODUCT_SOONG_NAMESPACES += \
     
 # Telephony
 PRODUCT_PACKAGES += \
-    ims-ext-common \
+    ims-ext-common_system \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
@@ -400,7 +400,7 @@ PRODUCT_PACKAGES += \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
-    ims-ext-common \
+    ims-ext-common_system \
     telephony-ext
     
 # TextClassifier
